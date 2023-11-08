bc20a44e15f315bac16b8930e2bfb489dee58021
JoaoLucasKnight <kirajota1@gmail.com>
2023-11-08 07:26:38 -0300

    conectando data.json / login feito

--- src/controller/constrole.js
+++ src/controller/constrole.js
@@ -1,9 +1,37 @@
-
+const dados = require('../data.json');
 
 function index(req,res){
-    res.render('homePage.html');
+    //const user1 = dados.user[0]
+    res.render('index.html');
 };
 
+function login(req, res){
+    const { username, password } = req.body; // Supondo que você está enviando essas informações no corpo da solicitação POST
+  
+    // Encontre o usuário com base no email (username)
+    const user = dados.user.find((user) => user.username === username);
+  
+    if (!user) {
+      // Usuário não encontrado
+      //res.status(401).json({ message: 'Usuário não encontrado' });
+      res.render('index.html');
+      return ;
+    }
+  
+    if (user.password !== password) {
+      // Senha incorreta
+      //res.status(401).json({ message: 'Senha incorreta' });
+      res.render('index.html');
+      return ;
+    }
+  
+    // Autenticação bem-sucedida
+    //res.status(200).json({ message: 'Login bem-sucedido', user });
+    return res.render('homePage.html');
+  };
+  
+
 module.exports ={
-    index
+    index,
+    login
 };
--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -4,5 +4,6 @@ const rota = express.Router();
 const controle = require('../controller/constrole');
 
 rota.get('/', controle.index );
+rota.post('/login', controle.login );
 
 module.exports = rota;
