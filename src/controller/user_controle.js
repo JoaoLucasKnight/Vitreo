29c53fce206df05f1e6f5d794d08172721563415
JoaoLucasKnight <kirajota1@gmail.com>
2023-11-19 07:44:43 -0300

    evolindo

--- src/controller/user_controle.js
+++ src/controller/user_controle.js
@@ -1,11 +1,8 @@
 const User = require('../model/user')
 
-function index(req,res){
-    //const user1 = dados.user[0]
-    res.render('index.html');
-};
 
-async function login(req, res){
+// ROTAS DE MANIPULAÇÃO DE DADOS 
+  async function login(req, res){
    
   const usuario = await User.findOne({
     where: {
@@ -16,52 +13,71 @@ async function login(req, res){
 
   if (usuario != null){
       req.session.usuario = usuario
+      req.session.autorizado = true
       res.redirect('/home')
   }
   else {
+    req.session.autorizado = false
     res.render('index.html')
+  }   
+  };
+
+  function verificar (req, res, next){
+  if(req.session.autorizado){
+    next();
+    }
+  else{
+    res.redirect('/');
+}   
+  };
+
+  function cadastrar(req,res){
+  let user = {
+    email: req.body.email,
+    senha: req.body.password,
+    telefone: req.body.Telefone
   }
-  
-   
+
+  User.create(user).then(()=>{
+    let sucesso = true;
+    res.render("homePage.html", {sucesso});
+}).catch((err)=>{
+    console.log(err);
+    let erro = true;
+    res.render("cadastro.html", {erro});
+});
+
+
   };
 
+// ROTAS BASICAS 
+  function index(req,res){
+  res.render('index.html');
+  };
   function home(req,res){
-    res.render('homePage.html')
+    const user = req.session.usuario
+    res.render('homePage.html', {user})
   };
-
   function conta(req,res){
     res.render('conta.html')
   };
   function config(req,res){
     res.render('config.html')
   };
-
   function cadastro(req,res){
     res.render('cadastro.html')
   };
-
   function add(req,res){
     res.render('add.html')
   };
-
-  function cadastrar(req,res){
-    let user = {
-      email: req.body.email,
-      senha: req.body.password,
-      telefone: req.body.Telefone
-    }
-
-    User.create(user).then(()=>{
-      let sucesso = true;
-      res.render("homePage.html", {sucesso});
-  }).catch((err)=>{
-      console.log(err);
-      let erro = true;
-      res.render("cadastro.html", {erro});
-  });
+  function forum(req,res){
+    res.render('forum.html')
+  };
+  function favorito(req,res){
+    res.render('favorito.html')
+  };
 
 
-  }
   
 
 module.exports ={
@@ -72,5 +88,8 @@ module.exports ={
     home,
     conta,
     config,
-    add
+    add, 
+    forum,
+    favorito,
+    verificar
 };
--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -7,10 +7,13 @@ const controle_user = require('../controller/user_controle');
 rota.get('/', controle_user.index );
 rota.post('/login', controle_user.login );
 rota.post('/cadastrar', controle_user.cadastrar);
-rota.get('/home', controle_user.home)
-rota.get('/conta', controle_user.conta)
-rota.get('/config', controle_user.config)
-rota.get('/add', controle_user.add)
+rota.get('/home', controle_user.verificar ,controle_user.home);
+rota.get('/conta', controle_user.verificar ,controle_user.conta);
+rota.get('/config', controle_user.verificar ,controle_user.config);
+rota.get('/add', controle_user.verificar ,controle_user.add);
+rota.get('/forum', controle_user.verificar ,controle_user.forum);
+rota.get('/favorito', controle_user.verificar ,controle_user.favorito);
+
 
 rota.get('/cadastro', controle_user.cadastro);
 
